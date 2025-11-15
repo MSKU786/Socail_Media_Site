1336f95755799f573e067a81ffc3892a3db2bcfa
MSKU786 <manishsingh332000@gmail.com>
2025-11-15 15:15:14 +0530

    Setting up acconts app added froms templates and viewss

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from djanog.views.generic import CreateView
+from . import forms
 
 # Create your views here.
+class SignUp(CreateView):
+  form_class = forms.UserCreationForm
+  success_url = reverse_lazy('login')
+  template_name = 'accounts/signup.html'
+  
+  
