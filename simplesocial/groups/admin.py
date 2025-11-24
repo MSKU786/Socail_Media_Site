0fe581be26c9783fb37bf6ecd8834d1d18666c11
MSKU786 <manishsingh332000@gmail.com>
2025-11-24 10:34:44 +0530

    Updated urls.py and admin.py for goupss app

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class GroupMemberInLine(admin.TabularInline):\
+  model = model.GroupMember
+
+admin.site.register(models.Group)
